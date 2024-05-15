cdb90df7ab1d88d209136fa313d22762107d0da0
Angelo <angelo.pucci@outlook.de>
2024-05-15 13:23:56 +0200

    Add debugging for environment variables

--- backend/settings.py
+++ backend/settings.py
@@ -25,6 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+
+# List all environment variables
+for key, value in os.environ.items():
+    print(f"{key}: {value}")
+
+    
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 print(f"SECRET_KEY: {SECRET_KEY}")

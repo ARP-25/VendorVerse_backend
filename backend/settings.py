6d3e88995b3ad17d1beb62262562f2e5baca7d00
Angelo <angelo.pucci@outlook.de>
2024-06-21 03:14:20 +0200

    Add prod frontend to allowed origins

--- backend/settings.py
+++ backend/settings.py
@@ -202,7 +202,7 @@ CORS_ALLOW_ALL_ORIGINS = False
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    "https://main--vendorverse.netlify.app",
+    "https://vendorverse.netlify.app",
     "https://vendorversebackend-production.up.railway.app",
 ]
 
